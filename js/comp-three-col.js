be21b1512f82d803f5fb647fe617fa4e3a1dc195
bayreporta <bayreporta@gmail.com>
2014-06-12 18:31:54 -0700

    refined

--- js/comp-three-col.js
+++ js/comp-three-col.js
@@ -4,14 +4,14 @@
 	var tabletopTest = false;
 	var compareControl = [];
 	var compareData = [[],[],[],[]];
-	var public_spreadsheet_url = "https://docs.google.com/spreadsheet/pub?key=0AnZDmytGK63SdGduSjNfbzlUOF9TTnRxeW9vNDF3SEE&output=html";
+	var public_spreadsheet_url = "https://docs.google.com/spreadsheets/d/1TDZveGDG_hYmXFlzaAPJUEpcr6KBWNAX_OCk0M8fn_Q/pubhtml";	
 
 
 // ------- TABLETOP ------- //
 	$(document).ready(function(){
 			Tabletop.init( { key: public_spreadsheet_url,
 		                     callback: showInfo,
-		                     wanted: ["control","tab1","tab2","tab3","tab4"],
+		                     wanted: ["control","tab1"],
 		                     debug: true } );
 	});
 
@@ -34,31 +34,6 @@
 			compareData[0].push(insertRow)
 		});
 		
-		$.each(tabletop.sheets("tab2").all(), function(i, tab2){
-			var insertRow = [[]];
-			insertRow[0] = tab2.left;
-			insertRow[1] = tab2.center;
-			insertRow[2] = tab2.right;
-			compareData[1].push(insertRow)
-		});
-		
-		$.each(tabletop.sheets("tab3").all(), function(i, tab3){
-			var insertRow = [[]];
-			insertRow[0] = tab3.left;
-			insertRow[1] = tab3.center;
-			insertRow[2] = tab3.right;
-			compareData[2].push(insertRow)
-		});
-		
-		$.each(tabletop.sheets("tab4").all(), function(i, tab4){
-			var insertRow = [[]];
-			insertRow[0] = tab4.left;
-			insertRow[1] = tab4.center;
-			insertRow[2] = tab4.right;
-			compareData[3].push(insertRow)
-		});
-		
-		//begin process of populating tabs, detecting number of tabs, etc.
 		verifyData();
 	}
 
@@ -67,52 +42,26 @@
 
 	function verifyData(){
 		var x = 0;
-		
-		//reads what tabs are activated in spreadsheet
 		for (i = 1 ; i < 5 ; i++){
 			var e = i - 1;
-			console.log(compareControl[0][i])
-			if (compareControl[0][i] === "no" || compareControl[0][i] === "No" || compareControl[0][i] === undefined || compareControl[0][i] === null){
+			if (compareControl[0][i] === "no"){
 				$("#compare-content-" + e).remove();
-				$(".compare-nav-tab[data='" + e + "']").remove();
 			}
 			else {
 				x = x + 1; 
 			}	
 		}
-
-		//set margins for nav buttons
-		switch(x){
-			case 1:
-				$(".compare-nav-tab").remove();
-				break;
-			case 2:
-				$(".compare-nav-tab").css("margin","0 90px");
-				break;
-			case 3:
-				$(".compare-nav-tab").css("margin","0 38px");
-				break;
-			case 4:
-				$(".compare-nav-tab").css("margin","0 12px");
-				break;
-		}
 		populateData(x);
 	}
 
 	function populateData(numTabs){
-		//determine max tabs
-		maxTabs = numTabs;
-		
 		//populate tabs with spreadsheet information
 		for (i = 0 ; i < numTabs ; i++){
 			var e = i + 1;
-			
-			//populate headers for columns in each tab
+			maxTabs = maxTabs + 1;
 			$("#compare-content-" + i + " h4:eq(0)").html(compareControl[1][e]);
 			$("#compare-content-" + i + " h4:eq(1)").html(compareControl[2][e]);
 			$("#compare-content-" + i + " h4:eq(2)").html(compareControl[3][e]);
-			
-			//populate content for each tab
 			seedTable(i)		
 		}
 
@@ -122,12 +71,12 @@
 		$(".compare-content-right").attr("style","width:" + compareControl[11][1] +"px");
 		$(".compare-content-row td").css("width",compareControl[10][1] +"px");
 		$(".compare-content-row td:eq(0)").css("width",compareControl[9][1] +"px");
-		$(".compare-content-row td:eq(2)").css("width",compareControl[11][1] +"px");
+		$(".compare-content-row td:eq(" + maxTabs + ")").css("width",compareControl[11][1] +"px");
 
 		//graphic information
 		$("#compare-header h1").html(compareControl[5][1]);
 		$("title").html(compareControl[5][1]);
-		$("#compare-header p:eq(0)").html(compareControl[8][1]);
+		$("#compare-header p:eq(0)").html("By " + compareControl[8][1]);
 		$("#compare-header p:eq(1)").html(compareControl[6][1]);
 		$("meta[name='description']").attr("content", compareControl[6][1]);
 		$("#compare-header p:eq(2)").html("Source: " + compareControl[7][1]);
